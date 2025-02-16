3d03c6ae77945ce27c93997ad41a94581311b6af
2060485 <2060485@collegemv.qc.ca>
2025-02-16 16:04:38 -0500

    add put and delete plants

--- src/routes/plant.route.ts
+++ src/routes/plant.route.ts
@@ -7,5 +7,7 @@ const plantRoutes = Router();
 plantRoutes.get('/:id', GameController.getPlant);
 plantRoutes.get('/', GameController.getAllPlants);
 plantRoutes.post('/', GameController.postPlant);
+plantRoutes.put('/:id', GameController.updatePlant);
+plantRoutes.delete('/:id', GameController.deletePlant);
 
 export default plantRoutes;
