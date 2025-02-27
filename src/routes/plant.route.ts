4eb510b7ca7eebb7dad4f0acae52713f77cb1f28
Mathis Normand <2060485@collegemv.qc.ca>
2025-02-27 13:54:03 -0500

    ajout middleware

--- src/routes/plant.route.ts
+++ src/routes/plant.route.ts
@@ -1,18 +1,19 @@
 import { Router } from "express";
 import { PlantController } from "../controllers/plant.controller";
 import { verifyToken } from "../middlewares/auth.middleware";
-
+import { adminRole, allRole } from '../utils/role.util';
+import { roleMiddleware } from '../middlewares/role.middleware';
 
 const plantRoutes = Router();
 
 plantRoutes.get('/user', verifyToken, PlantController.getPlantsByUser);
 plantRoutes.get('/captor',verifyToken, PlantController.getCaptorsByUser);
 plantRoutes.get('/captorInfo',verifyToken, PlantController.getCaptorsInfoByUser);
-plantRoutes.get('/:id', PlantController.getPlant);
-plantRoutes.get('/', PlantController.getAllPlants);
-plantRoutes.post('/', PlantController.postPlant);
-plantRoutes.put('/:id', PlantController.updatePlant);
-plantRoutes.delete('/:id', PlantController.deletePlant);
+plantRoutes.get('/:id',verifyToken, PlantController.getPlant);
+plantRoutes.get('/',verifyToken,roleMiddleware(adminRole), PlantController.getAllPlants);
+plantRoutes.post('/',verifyToken, PlantController.postPlant);
+plantRoutes.put('/:id',verifyToken, PlantController.updatePlant);
+plantRoutes.delete('/:id',verifyToken, PlantController.deletePlant);
 
 
 export default plantRoutes;
